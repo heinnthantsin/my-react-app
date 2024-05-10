f82e5f7e64998cf331ca28af69a3d41ad30acd84
heinnthantsin <heinnthant01@gmail.com>
2024-05-10 07:20:44 +0630

    ts config

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -29,6 +30,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
+
   )
 }
 
